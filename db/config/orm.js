68a5b79c1759e97f0b2ff8043317708f06798d43
Jeff Hinzmann <JAHinzmann@gmail.com>
2018-05-21 18:49:31 -0400

    finished orm.js and worked on burger.js

--- db/config/orm.js
+++ db/config/orm.js
@@ -1,8 +1,8 @@
 const connection = require('connection.js');
 
 var orm = {
-    create: function(table name, column name, value name, callback) {
-        var queryString =
+    create: function(tableName, columnName1, columnName2, valueName, callback) {
+        var queryString = "INSERT INTO" + tableName + "(" + columnName1 + "," + columnName2 + ") VALUES (" + valueName + ", false);" 
         connection.query(queryString, function(error, result){
             if (error) {
                 throw error
@@ -10,11 +10,23 @@ var orm = {
             callback(result);
         });
     },
-    read: function(table name, callback){
-
+    read: function(tableName, callback){
+        var queryString = "SELECT * FROM" + tableName + ";"
+        connection.query(queryString, function(error, result){
+            if (error) {
+                throw error
+            }; 
+            callback(result);
+        });
     },
-    update: function(table name, column name, value name, callback){
-
+    update: function(tableName, columnName, valueName, callback){
+        var queryString = "UPDATE" + tableName + "SET" + columnName + "= false WHERE ID=" + valueName + ";"
+        connection.query(queryString, function(error, result){
+            if (error) {
+                throw error
+            }; 
+            callback(result);
+        });
     },
 }
 
--- db/models/burger.js
+++ db/models/burger.js
@@ -1,1 +1,21 @@
 const orm = require('../config/orm.js');
+
+var burger = {
+    create: function(vals, cb) {
+        orm.create("burgers", "burger_name", "devoured", vals, function(res) {
+          cb(res);
+        });
+      },
+    read: function(cb) {
+        orm.read("burgers", function(res) {
+          cb(res);
+        });
+      },
+    update: function(vals, cb) {
+        orm.create("burgers", "devoured", vals, function(res) {
+          cb(res);
+        });
+      },
+}
+
+module.exports = burger;
